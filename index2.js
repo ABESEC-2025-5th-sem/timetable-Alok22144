b3d2ba1366d183b368e3bb6ddba597f55dfc1464
Alok22144 <alokguptag421@gmail.com>
2025-09-17 09:34:30 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,6 @@
+function colorHandler(){
+            console.log(color);
+            console.log(document.getElementByID("bulb"));
+            const bulb=document.getElementById("bulb")
+            bulb.style.backgroundColor=Color
+        }
